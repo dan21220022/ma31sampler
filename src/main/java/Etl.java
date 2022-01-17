e080e3d77ab393ce539c7dbeb7324a3b56e08fe0
dan21220022 <dan21220022@gmail.com>
2022-01-17 08:14:45 +0200

    added Etl

---
+++ src/main/java/Etl.java
@@ -0,0 +1,24 @@
+public class Etl {
+    private Extractor extractor;
+    private Transformer transformer;
+    private Loader loader;
+    private String srcPath;
+    private String destPath;
+
+    Etl(Extractor extractor, Transformer transformer, Loader loader, String srcPath, String destPath)
+    {
+        this.extractor = extractor;
+        this.transformer = transformer;
+        this.loader = loader;
+        this.srcPath = srcPath;
+        this.destPath = destPath;
+    }
+
+    public void processData()
+    {
+        DataContainer dataContainer;
+        dataContainer = extractor.extract(srcPath);
+        dataContainer = transformer.transform(dataContainer);
+        loader.load(dataContainer, destPath);
+    }
+}
