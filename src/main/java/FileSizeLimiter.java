d636386b79c1518a779572a97da80eb17cc34fa1
dan21220022 <dan21220022@gmail.com>
2022-01-18 17:02:22 +0200

    implemented FileSizeLimiter

--- src/main/java/FileSizeLimiter.java
+++ src/main/java/FileSizeLimiter.java
@@ -8,6 +8,22 @@ public class FileSizeLimiter extends Limiter{
 
     @Override
     public ArrayList<ArrayList<DataContainer>> getLimitedList(ArrayList<DataContainer> container) {
-        return null;
+        int sizeCounter = 0;
+        ArrayList<ArrayList<DataContainer>> dataContainers = new ArrayList<>();
+        int currentContainersListIndex = 0;
+        dataContainers.add(new ArrayList<DataContainer>());
+
+        for(int i = 0; i < container.size(); i++)
+        {
+            dataContainers.add(new ArrayList<DataContainer>());
+            dataContainers.get(currentContainersListIndex).add(container.get(i));
+            sizeCounter += container.get(i).getData().toString().length();
+            if(sizeCounter >= limit)
+            {
+                sizeCounter = 0;
+                currentContainersListIndex++;
+            }
+        }
+        return dataContainers;
     }
 }
